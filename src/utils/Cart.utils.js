0e97b183aeb941e1a12e68ae9d171d7617972d5c
rohan-cpi <sharma.rohan18@gmail.com>
2021-10-14 12:35:16 +0530

    meta tags fixes

--- src/utils/Cart.utils.js
+++ src/utils/Cart.utils.js
@@ -1,20 +1,3 @@
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToAdd.id
-  );
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === cartItemToAdd.id
-        ? {
-            ...cartItem,
-            quantity: cartItem.quantity + 1,
-          }
-        : cartItem
-    );
-  }
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-};
-
 export const RemoveFromCart = (cartItems, ItemToDelete) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === ItemToDelete.id
